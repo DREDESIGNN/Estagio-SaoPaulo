e9db9c229ac6e5d165bb3c448f21684223b6fc33
ANDRÉ LUIZ COSTA DE SOUZA <32402573+DREDESIGNN@users.noreply.github.com>
2024-09-02 16:20:15 -0300

    Add files via upload
    Questão 1:
    Passo a passo do código:
    Inicialização:
    
    INDICE é definido como 13.
    SOMA é inicializada com 0.
    K é inicializada com 0.
    Loop (Enquanto K < INDICE):
    
    A variável K é incrementada em 1 a cada iteração (K = K + 1).
    O valor de K é adicionado à variável SOMA a cada iteração (SOMA = SOMA + K).
    Condições de execução:
    
    O loop continua até que K seja igual a 13.
    Cálculo:
    Vamos fazer as somas manualmente para cada iteração:
    
    Quando K = 1: SOMA = 0 + 1 = 1
    Quando K = 2: SOMA = 1 + 2 = 3
    Quando K = 3: SOMA = 3 + 3 = 6
    Quando K = 4: SOMA = 6 + 4 = 10
    Quando K = 5: SOMA = 10 + 5 = 15
    Quando K = 6: SOMA = 15 + 6 = 21
    Quando K = 7: SOMA = 21 + 7 = 28
    Quando K = 8: SOMA = 28 + 8 = 36
    Quando K = 9: SOMA = 36 + 9 = 45
    Quando K = 10: SOMA = 45 + 10 = 55
    Quando K = 11: SOMA = 55 + 11 = 66
    Quando K = 12: SOMA = 66 + 12 = 78
    Quando K = 13: SOMA = 78 + 13 = 91
    
    O programa soma todos os números de 1 até 13, resultando em 91.
    
    Resultado:
    Ao final do processamento, o valor da variável SOMA será 91.

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int INDICE = 13, SOMA = 0, K = 0;
+
+    while (K < INDICE) {
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+
+    printf("%d\n", SOMA);
+
+    return 0;
+}
