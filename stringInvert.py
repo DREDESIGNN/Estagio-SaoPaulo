2c395e6119aec74e6e91fa5879687a4e62167d8a
ANDRÉ LUIZ COSTA DE SOUZA <32402573+DREDESIGNN@users.noreply.github.com>
2024-09-03 23:04:23 -0300

    questao05.py
    Explicação:
    
    -A função 'inverter_string' recebe uma string como argumento.
    
    -A expressão 'string[::-1]' é usada para inverter a string. Isso funciona porque o slicing '[start:stop:step]' permite especificar o início, o fim e o passo do fatiamento. Ao usar '-1' como passo, você inverte a string.
    
    -O programa então imprime a string original e a string invertida.

---
+++ stringInvert.py
@@ -0,0 +1,9 @@
+# Questão 05 - Estagio - São Paulo
+def inverter_string(string):
+    return string[::-1]
+
+# Testando a função
+texto = "Olá, André!"
+texto_invertido = inverter_string(texto)
+print("String original:", texto)
+print("String invertida:", texto_invertido)
